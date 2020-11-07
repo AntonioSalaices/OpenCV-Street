894a360e64bc8c8bc0c668a1309a732fffd2e453
AntonioSalaices <antonio.salaicesm@gmail.com>
2020-11-07 10:30:14 -0700

    practicando con escala de grisess

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import cv2
+# Importamos las librerías necesarias
 import numpy as np
+import cv2
+import time
 
-from matplotlib import pyplot as plt
-
-img = cv2.imread('img/profile.jpg',cv2.IMREAD_COLOR)
-cv2.rectangle(img, (55,55), (200,150), (0,255,0), 5)
-cv2.imshow('image',img)
-cv2.waitKey(0)
+captura = cv2.VideoCapture(0)
+while (captura.isOpened()):
+  ret, imagen = captura.read()
+  gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+  
+  if ret == True:
+    rectangle = cv2.rectangle(imagen, (80,100),(100,-100), (0,255,0), 10)
+    cv2.imshow('video', imagen)
+    cv2.imshow('video a escala de grises', gris)
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+      break
+  else: break
+captura.release()
 cv2.destroyAllWindows()
-
